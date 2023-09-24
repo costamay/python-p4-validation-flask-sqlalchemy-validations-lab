268124d252c6b7f8083c1df0046c4781b822c590
costamay <titusouko@gmail.com>
2023-09-24 12:31:43 +0300

    Complete

--- server/models.py
+++ server/models.py
@@ -8,9 +8,21 @@ class Author(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name= db.Column(db.String, unique=True, nullable=False)
-    phone_number = db.Column(db.String)
+    phone_number = db.Column(db.String(10))
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+    @validates('name')
+    def validate_name(self, key, value):
+        if not value:
+            raise ValueError("requires each record to have a name.")
+        return value
+    
+    @validates('phone_number')
+    def validate_phone_number(self, key, value):
+        if value and len(value) != 10:
+            raise ValueError("Phone number must be exactly ten digits.")
+        return value
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -26,6 +38,33 @@ class Post(db.Model):
     summary = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+    
+    @validates('category')
+    def validate_category(self, key, value):
+        if value not in ["Fiction", "Non-Fiction"]:
+            raise ValueError("Category must be either 'Fiction' or 'Non-Fiction'.")
+        return value
+    
+    @validates('content')
+    def validate_content(self, key, value):
+        if value and len(value) < 250:
+            raise ValueError("Content must be at least 250 characters long.")
+        return value
+    
+    @validates('summary')
+    def validate_summary(self, key, value):
+        if value and len(value) >= 250:
+            raise ValueError("Summary cannot exceed 250 characters.")
+        return value
+    
+    @validates('title')
+    def validate_clickbait_title(self, key, value):
+        clickbait_phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(phrase in value for phrase in clickbait_phrases):
+            raise ValueError("Title must contain at least one of the following: 'Won't Believe', 'Secret', 'Top [number]', 'Guess'")
+        return value
+
 
 
     def __repr__(self):
